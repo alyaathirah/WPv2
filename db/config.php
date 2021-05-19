76953536b2ad0eee6b75c12ba0a8352b9493c6aa
nurulsyf <you@example.com>
2021-05-19 08:46:28 +0800

    add config.php

--- db/config.php
+++ db/config.php
@@ -1,20 +1,22 @@
 <?php
 //Step 1: Connecting to a Database using PDO API
-// modify these variables for your installation
-try {	
-	$connectionString = "mysql:host=localhost:3325;dbname=shopping_list";
-	$databaseUsername = 'root';
-	$databasePassword = '';
+// modify these variables for your installation 
+        try {	
+        
+            $connectionString = "mysql:host=localhost:3325;dbname=shoppingllist";
+            $databaseUsername = 'root';
+            $databasePassword = '';
 
-	$pdo = new PDO($connectionString, $databaseUsername, $databasePassword);
-	// set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-    echo "Database connected successfully <br>"; 
-	
-    }
-catch(PDOException $e) {
-    echo "Database connection failed: " . $e->getMessage();
-    }
+            $pdo = new PDO($connectionString, $databaseUsername, $databasePassword);
+            // set the PDO error mode to exception
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            echo "Database connected successfully <br>"; 
+        
+        }
+        catch(PDOException $e) {
+            echo "Database connection failed: " . $e->getMessage();
+        }
+
 
 ?>
