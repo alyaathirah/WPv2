be7979081e2a3b3f5e786cabc4d8f88122472b47
syakinah00 <81899440+syakinah00@users.noreply.github.com>
2021-06-11 00:52:17 +0800

    user connection

---
+++ config.php
@@ -0,0 +1,42 @@
+<?php
+/*
+//Sample MySQLi config file
+ */
+//Step 1: Connecting to a Database using MySQLi API 
+// modify these variables for your installation
+$databaseHost = 'localhost';
+$databaseName = 'setting';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+
+//MySQLi Procedural
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+//MySQLi Object-Oriented approach
+/*
+$mysqli = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+*/
+
+//Step 2:Handling connection error-mysqli
+//MySQLi Procedural approach
+// mysqli_connect_errno returns the last error code
+if ( mysqli_connect_errno() ) {
+	// die() is equivalent to exit()
+	die( "Database connection failed: " . mysqli_connect_error() );
+} 
+
+//Step 2: Handling Connection Errors - mysqli 
+//MySQLi Object-Oriented approach
+//connect_errno returns the last error code
+// Check connection
+/*
+if ($mysqli->connect_error) {
+	die("Connection failed: " . $mysqli->connect_error);
+}
+*/
+
+echo "Database connected successfully <br>";
+
+
+?>
