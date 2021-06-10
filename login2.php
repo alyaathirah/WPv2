9e366905b717f3485059ef03df687dbab19ad1a8
alyaathirah <62088501+alyaathirah@users.noreply.github.com>
2021-06-11 07:07:26 +0800

    Update login2.php

--- login2.php
+++ login2.php
@@ -1,68 +1,12 @@
 <!DOCTYPE html>
 
-<?php 
-session_start(); 
-include "config.php";
-
-if (isset($_POST['uname']) && isset($_POST['password'])) {
-
-	$uname = $_POST['uname'];
-	$pass = $_POST['password'];
-
-	if (empty($uname)) {
-		header("Location: login1.php?error=User Name is required");
-	    exit();
-	}else if(empty($pass)){
-        header("Location: login1.php?error=Password is required");
-	    exit();
-	}else{
-		        
-		$sql = "SELECT * FROM users2 WHERE Username='$uname' AND Password1='$pass'";
-
-		$result = mysqli_query($mysqli, $sql);
-
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['Username'] === $uname && $row['Password1'] === $pass) {
-            	$_SESSION['id'] = $row['id'];
-                $_SESSION['FirstName'] = $row['FirstName'];
-                $_SESSION['LastName'] = $row['LastName'];
-                $_SESSION['Username'] = $row['Username'];
-            	$_SESSION['Email'] = $row['Email'];
-            	$_SESSION['Bio'] = $row['Bio'];
-                $_SESSION['PhoneNumber'] = $row['PhoneNumber'];
-            	$_SESSION['Birthday'] = $row['Birthday'];
-            	$_SESSION['Address1'] = $row['Address1'];
-                $_SESSION['City'] = $row['City'];
-                $_SESSION['State1'] = $row['State1'];
-            	$_SESSION['Zip'] = $row['Zip'];
-            	$_SESSION['Password1'] = $row['Password1'];
-				$_SESSION['images'] = $row['images'];
-            	header("Location: home.php");
-		        exit();
-            }else{
-				header("Location: login1.php?error=Incorect User name or password");
-		        exit();
-			}
-		}else{
-			header("Location: login1.php?error=Incorect User name or password");
-	        exit();
-		}
-	}
-	
-}else{
-	header("Location: login1.php");
-	exit();
-}
-?>
-
 <html lang="en">
   <head>
     <meta charset="utf-8" />
 
     <link rel="icon" href="images/icon.png" type="image/x-icon" />
     <link rel="stylesheet" href="css/styleLogin.css" />
-    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
 
     <script type="text/javascript" src="js/login.js"></script>
     <script type="text/javascript" src="js/userList.js"></script>
@@ -85,10 +29,10 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         <form>
           <br />
           <label>Email:</label><br />
-          <input id="email" type="mail" placeholder="Email" />
+          <input id="email" type="mail" placeholder="Email" name="email"/>
           <br /><br />
           <label>Password:</label><br />
-          <input id="password" type="password" placeholder="Password" />
+          <input id="password" type="password" placeholder="Password" name="password"/>
           <br /><br />
           <button type="button" onclick="Login()">
             <!-- go to login function -->
@@ -227,3 +171,59 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
     </div>
   </div>
 </html>
+
+<?php 
+session_start(); 
+include "config.php";
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+
+	$email = $_POST['email'];
+	$pass = $_POST['password'];
+
+	if (empty($email)) {
+		header("Location: login1.php?error=Email is required");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: login1.php?error=Password is required");
+	    exit();
+	}else{
+		        
+		$sql = "SELECT * FROM users2 WHERE Username='$email' AND Password1='$pass'";
+
+		$result = mysqli_query($mysqli, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['Username'] === $email && $row['Password1'] === $pass) {
+            	$_SESSION['id'] = $row['id'];
+                $_SESSION['FirstName'] = $row['FirstName'];
+                $_SESSION['LastName'] = $row['LastName'];
+                $_SESSION['Username'] = $row['Username'];
+            	$_SESSION['Email'] = $row['Email'];
+            	$_SESSION['Bio'] = $row['Bio'];
+                $_SESSION['PhoneNumber'] = $row['PhoneNumber'];
+            	$_SESSION['Birthday'] = $row['Birthday'];
+            	$_SESSION['Address1'] = $row['Address1'];
+                $_SESSION['City'] = $row['City'];
+                $_SESSION['State1'] = $row['State1'];
+            	$_SESSION['Zip'] = $row['Zip'];
+            	$_SESSION['Password1'] = $row['Password1'];
+				$_SESSION['images'] = $row['images'];
+            	header("Location: home.php");
+		        exit();
+            }else{
+				header("Location: login1.php?error=Incorect User name or password");
+		        exit();
+			}
+		}else{
+			header("Location: login1.php?error=Incorect User name or password");
+	        exit();
+		}
+	}
+	
+}else{
+	header("Location: login1.php");
+	exit();
+}
+?>
