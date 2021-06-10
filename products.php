56466ac4ce77019275b10bfeadf877361a87bdfa
nurulsyfiqah <nurulsyfiqah25@gmail.com>
2021-06-10 16:19:42 +0800

    factor modal part

--- products.php
+++ products.php
@@ -402,54 +402,18 @@
       </div>
         <!-- Profile Modal -->
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div class="modal-dialog">
+        <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
                 <h4 class="modal-title" id="staticBackdropLabel" style="color: #a82c21; font-size:40px;">Profile</h4>
                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><i class="fa fa-times" style="font-size: 35px;"></i></button>
               </div>
-              <div class="modal-body container">
-                <img src="<?php echo $images;?>" class="wrapper" alt="profile photo" id="profile photo">
-                <div class="row"   >
-                    <div class="col-sm-4">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">First Name</label><br>   
-                      <label class="text-muted" id="FName" style="text-indent: 30px;"><?php echo $FName; ?></label>
-                    </div>
-                    <div class="col-sm-4">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">Last Name</label><br>   
-                      <label class="text-muted" id="LName" style="text-indent: 30px;"><?php echo $LName; ?></label>
-                    </div>
-                    <div class="col-sm-4">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">Username</label><br>   
-                      <label class="text-muted" id="UName" style="text-indent: 30px;">@<?php echo $UName; ?></label>
-                    </div>
-                    <div class="col-sm-6">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">Email</label><br>   
-                      <label class="text-muted" id="Email" style="text-indent: 30px;"><?php echo $Email; ?></label>
-                    </div>
-                    <div class="col-sm-6">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">Bio</label> <br>  
-                      <label class="text-muted" id="Email" style="text-indent: 30px;"><?php echo $Bio; ?></label>
-                    </div>
-                    <div class="col-sm-3">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">Gender</label><br>   
-                      <label class="text-muted" id="Gender" style="text-indent: 30px;"><?php echo $Gender; ?></label>
-                    </div>
-                    <div class="col-sm-5">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">Phone Number</label><br>  
-                      <label class="text-muted" id="PNumber" style="text-indent: 30px;"><?php echo $PNumber; ?></label>
-                    </div>
-                    <div class="col-sm-4">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">Birthday</label><br>   
-                      <label class="text-muted" id="Birthday" style="text-indent: 30px;"><?php echo $Birthday; ?></label>
-                    </div>
-                    <div class="col-sm-12">
-                      <label style="font-weight:bold; color:maroon; text-indent: 30px;">Address</label><br>   
-                      <label class="text-muted" id="Address" style="text-indent: 30px;"><?php echo $Address," ",$City," ",$Zip," ",$State; ?></label>
-                    </div>
-                <div>
-              </div>
-
+                <?php 
+                
+                require_once('testModal.php');
+                getModal($images,$FName,$LName,$UName,$Email,$Bio,$Gender,$PNumber,$Birthday,$Address,$City,$Zip,$State); 
+                ?>
+            </div>
               <?php 
                 }else{
                     header("Location: loginDummy.php");
