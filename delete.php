68768db794ac111453a9c3c5d855c3888f2234fd
syakinah00 <81899440+syakinah00@users.noreply.github.com>
2021-06-10 23:42:33 +0800

    delete_page

---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php
+//Example - MySQLi
+//Step 1. Connect to the database.
+//Step 2. Handle connection errors
+//including the database connection file
+session_start();
+include_once("config.php");
+
+if (isset($_SESSION['id']) && isset($_SESSION['Username'])) {
+
+//getting id of the data from url
+$id = $_SESSION['id'];
+
+//3. Execute the SQL query.
+//deleting the row from table
+$result = mysqli_query($mysqli, "DELETE FROM users2 WHERE id=$id");
+
+//Step 5: Freeing Resources and Closing Connection using mysqli
+mysqli_close($mysqli);
+
+//4. Process the results.
+//redirecting to the display page (index.php in our case)
+header("Location:login.php");
+}
+?>
+
