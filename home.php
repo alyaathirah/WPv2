1efe17a70f36c2d59501c39d01b00d4399c1c240
syakinah00 <81899440+syakinah00@users.noreply.github.com>
2021-06-15 03:13:28 +0800

    Update home.php

--- home.php
+++ home.php
@@ -26,45 +26,48 @@ if (isset($_SESSION['id']) && isset($_SESSION['Username'])) {
     $Password=$res['Password1'];
     $images=$res['images'];
     $Gender=$res['Gender'];
-	if(empty($FName) || empty($LName) || empty($UName) || empty($Email) || empty($Bio)
-			|| empty($PNumber) || empty($Birthday) || empty($Address) || empty($City) 
-			|| empty($State) || empty($State) || empty($Zip) || empty($Gender)){
-	      if(empty($FName)) {
-		$FName="Set Now";
-	      }
+    if(empty($FName) || empty($LName) || empty($UName) || empty($Email) || empty($Bio) || empty($PNumber) || empty($Birthday) || empty($Address) || empty($City) || empty($State) || empty($State) || empty($Zip) || empty($Gender)|| empty($images))
+    {
+      if(empty($images)) {
+        $images="images/default.png";
+      }
 
-	      if(empty($LName)) {
-		$LName="Set Now";
-	      }
+      if(empty($FName)) {
+        $FName="Set Now";
+      }
+      
+      if(empty($LName)) {
+        $LName="Set Now";
+      }
+      
+      if(empty($Bio)) {
+        $Bio="Set Now";
+      }
+  
+      if(empty($Address)) {
+        $Address="Set Now";
+      }	
+      
+      if(empty($State)) {
+        $State="";
+      }
+  
+      if(empty($City)) {
+        $City="";
+      }	
+      
+      if(empty($Zip)) {
+        $Zip="";
+      }
 
-	      if(empty($Bio)) {
-		$Bio="Set Now";
-	      }
+      if(empty($Gender)) {
+        $Gender="Set Now";
+      }
 
-	      if(empty($Address)) {
-		$Address="Set Now";
-	      }	
-
-	      if(empty($State)) {
-		$State="";
-	      }
-
-	      if(empty($City)) {
-		$City="";
-	      }	
-
-	      if(empty($Zip)) {
-		$Zip="";
-	      }
-
-	      if(empty($Gender)) {
-		$Gender="Set Now";
-	      }
-
-	      if(empty($PNumber)) {
-		$PNumber="Set Now";
-	      }
-	    }
+      if(empty($PNumber)) {
+        $PNumber="Set Now";
+      }
+    }
   }
   
 ?>
