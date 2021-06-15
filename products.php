00b35d85b6cd8fe68d4cc59db92547b767f7f767
alyaathirah <62088501+alyaathirah@users.noreply.github.com>
2021-06-16 04:04:00 +0800

    Update products.php

--- products.php
+++ products.php
@@ -117,6 +117,7 @@
             ><img src="images/logo.png" style="width: 200px; height: auto"
           /></a>
         </div>
+        <!-- MY LIST BUTTON -->
         <div class="col-4 d-flex justify-content-end align-items-center">
           <a
             class="btn btn-sm btn-outline-secondary"
