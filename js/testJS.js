905d3038a5b29d047a2dfd68693a3be0c5a5cb2f
nurulsyfiqah <nurulsyfiqah25@gmail.com>
2021-06-18 13:34:04 +0800

    update js

--- js/testJS.js
+++ js/testJS.js
@@ -1,18 +1,18 @@
-var url = "http://localhost/WPv2/WPv2/products.php";
+//var url = "http://localhost/Web%20Programming/Assignment/WPv2/products.php";
 var value = "";
 $(document).ready(function(){
     $(".dropdown-item").click(function(){
         value = this.textContent;
-        window.location.href = url+'?&category='+value;
+        window.location.href = 'products.php'+'?&category='+value;
     });
 });
 
-var url1 = "http://localhost/WPv2/WPv2/product_desc.php";
+//var url1 = "http://localhost/Web%20Programming/Assignment/WPv2/product_desc.php";
 var item_name = "";
 $(document).ready(function(){
     $(".btn-view").click(function(){
         item_id = this.id;
-        window.location.href = url1+'?&item_id='+item_id;
+        window.location.href = 'product_desc.php'+'?&item_id='+item_id;
     });
 });
 
